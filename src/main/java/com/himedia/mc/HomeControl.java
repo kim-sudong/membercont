3c18bfe32cb7b2078553a43198eeb9f30a2eb736
1234 <1234@192.168.0.70>
2024-09-30 09:56:30 +0900

    24.09.30

--- src/main/java/com/himedia/mc/HomeControl.java
+++ src/main/java/com/himedia/mc/HomeControl.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@CrossOrigin(origins="http://localhost:3000")
 public class HomeControl {
 	@Autowired DAO dao;
 	@Autowired BoardDAO bdao;
